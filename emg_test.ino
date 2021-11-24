e51c01382134360c9ebb0e02140424bdd0010ca7
Tanay Agrawal <59744252+makertanay@users.noreply.github.com>
2021-11-24 13:42:33 +0530

    Add files via upload

---
+++ emg_test.ino
@@ -0,0 +1,13 @@
+int EMGPin = A0;
+int EMGVal = 0;
+
+void setup() {
+Serial.begin(115200);
+pinMode(EMGPin,INPUT);
+}
+
+void loop() {
+EMGVal = analogRead(EMGPin);
+Serial.println(EMGVal);
+delay(50);
+}
